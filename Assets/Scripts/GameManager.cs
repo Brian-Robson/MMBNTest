af1cf052c882379ea5141daaf4f4fb65036c42b9
Brian Robson <brian.robson05@gmail.com>
2020-02-01 15:04:27 -0500

    Fixed some inefficient code.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     public GameObject playerCharacter;
     public GameObject testEnemy;
     public GameObject battleTile;
+
+    // Player Variables
+    public int playerHP;
+    public int playerMaxHP;
+
     GameObject[,] battleArray = new GameObject[3, 6];
 
     void Awake()
@@ -17,29 +22,23 @@ public class GameManager : MonoBehaviour
 
     void InitializeBattle() // Generates the battle tiles, the player object, enemies, etc.
     {
-        for (int y = 0; y < 3; y++) // Really nothing else I need to do with this
+        for (int y = 0; y < 3; y++) // This loop generates the 6x3 of tiles needed for combat
         {
             for (int x = 0; x < 6; x++)
             {
                 battleArray[y, x] = Instantiate(battleTile, new Vector3(x * 0.4f - 1, y * 0.24f, 0), Quaternion.identity);
+                if (x < 3) // Conditional sets tile ownership
+                {
+                    UpdateTileColour(battleArray[y, x], Color.red, true);
+                }
+                else
+                {
+                    UpdateTileColour(battleArray[y, x], Color.blue, false);
+                }
             }
         }
-        for (int y = 0; y < 3; y++) // Sets the player character's 3x3 tiles
-        {
-            for (int x = 0; x < 3; x++)
-            {
-                UpdateTileColour(battleArray[y, x], Color.red, true);
-            }
-        }
-        for (int y = 0; y < 3; y++) // Sets the enemy 3x3 tiles
-        {
-            for (int x = 3; x < 6; x++)
-            {
-                UpdateTileColour(battleArray[y, x], Color.blue, false);
-            }
-        }
-        GameObject temp = Instantiate(playerCharacter, GetDestination(1, 1), Quaternion.identity);
-        temp.GetComponent<PlayerController>().gameManager = this;
+        GameObject playerInstance = Instantiate(playerCharacter, GetDestination(1, 1), Quaternion.identity);
+        playerInstance.GetComponent<PlayerController>().Constructor(this, playerHP, playerMaxHP);
     }
 
     void UpdateTileColour(GameObject targetTile, Color targetColour, bool playerTile)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,55 +8,66 @@ public class PlayerController : MonoBehaviour
     public GameManager gameManager;
     int x = 1;
     int y = 1;
-    int maxHP = 100;
-    int curHP = 100;
+    public int maxHP;
+    public int HP;
     float moveDelay = 0.2f;
     float currMoveDelay = 0.0f;
+    bool alive = false; // Prevents function until properly initialized via Constructor function
 
     void Update()
     {
-        if (currMoveDelay > 0)
+        if (alive)
         {
-            currMoveDelay = currMoveDelay - 1 * Time.deltaTime;
+            if (currMoveDelay > 0)
+            {
+                currMoveDelay = currMoveDelay - 1 * Time.deltaTime;
+            }
+            if (currMoveDelay <= 0)
+            {
+                if (Input.GetKeyDown("up"))
+                {
+                    if (gameManager.ValidMove(y + 1, x))
+                    {
+                        transform.position = gameManager.GetDestination(y + 1, x);
+                        y = y + 1;
+                        currMoveDelay = moveDelay;
+                    }
+                }
+                else if (Input.GetKeyDown("down"))
+                {
+                    if (gameManager.ValidMove(y - 1, x))
+                    {
+                        transform.position = gameManager.GetDestination(y - 1, x);
+                        y = y - 1;
+                        currMoveDelay = moveDelay;
+                    }
+                }
+                else if (Input.GetKeyDown("left"))
+                {
+                    if (gameManager.ValidMove(y, x - 1))
+                    {
+                        transform.position = gameManager.GetDestination(y, x - 1);
+                        x = x - 1;
+                        currMoveDelay = moveDelay;
+                    }
+                }
+                else if (Input.GetKeyDown("right"))
+                {
+                    if (gameManager.ValidMove(y, x + 1))
+                    {
+                        transform.position = gameManager.GetDestination(y, x + 1);
+                        x = x + 1;
+                        currMoveDelay = moveDelay;
+                    }
+                }
+            }
         }
-        if (currMoveDelay <= 0)
-        {
-            if (Input.GetKeyDown("up")) 
-            {
-                if (gameManager.ValidMove(y + 1, x))
-                {
-                    transform.position = gameManager.GetDestination(y + 1, x);
-                    y = y + 1;
-                    currMoveDelay = moveDelay;
-                }
-            }
-            if (Input.GetKeyDown("down")) 
-            {
-                if (gameManager.ValidMove(y - 1, x))
-                {
-                    transform.position = gameManager.GetDestination(y - 1, x);
-                    y = y - 1;
-                    currMoveDelay = moveDelay;
-                }
-            }
-            if (Input.GetKeyDown("left"))
-            {
-                if (gameManager.ValidMove(y, x - 1))
-                {
-                    transform.position = gameManager.GetDestination(y, x - 1);
-                    x = x - 1;
-                    currMoveDelay = moveDelay;
-                }
-            }
-            if (Input.GetKeyDown("right"))
-            {
-                if (gameManager.ValidMove(y, x + 1))
-                {
-                    transform.position = gameManager.GetDestination(y, x + 1);
-                    x = x + 1;
-                    currMoveDelay = moveDelay;
-                }
-            }
-        }
+    }
+    public void Constructor(GameManager gameManager, int HP, int maxHP)
+    {
+        this.gameManager = gameManager;
+        this.HP = HP;
+        this.maxHP = maxHP;
+        this.alive = true;
     }
 }
